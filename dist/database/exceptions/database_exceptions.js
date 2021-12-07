f79510c1984035807556d783a2ad253ed248895b
Wesley Levasseur <mailpro.levasseur@icloud.com>
2021-12-07 01:36:53 +0100

    Refactoring code, Cleanup

--- dist/database/exceptions/database_exceptions.js
+++ dist/database/exceptions/database_exceptions.js
@@ -1,5 +1,5 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {value: true});
 exports.database_exceptions = void 0;
 var database_exceptions;
 (function (database_exceptions) {
@@ -9,26 +9,33 @@ var database_exceptions;
             Object.setPrototypeOf(this, DatabaseError.prototype);
         }
     }
+
     database_exceptions.DatabaseError = DatabaseError;
+
     class DatabaseNoSuchData extends Error {
         constructor(message) {
             super(message);
             Object.setPrototypeOf(this, DatabaseNoSuchData.prototype);
         }
     }
+
     database_exceptions.DatabaseNoSuchData = DatabaseNoSuchData;
+
     class UserPermissionNotEnough extends Error {
         constructor(message) {
             super(message);
             Object.setPrototypeOf(this, UserPermissionNotEnough.prototype);
         }
     }
+
     database_exceptions.UserPermissionNotEnough = UserPermissionNotEnough;
+
     class UserPermissionAlready extends Error {
         constructor(message) {
             super(message);
             Object.setPrototypeOf(this, UserPermissionAlready.prototype);
         }
     }
+
     database_exceptions.UserPermissionAlready = UserPermissionAlready;
 })(database_exceptions = exports.database_exceptions || (exports.database_exceptions = {}));
--- dist/test.js
+++ dist/test.js
@@ -1,5 +1,5 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {value: true});
 const puresystem_sdk_1 = require("./puresystem_sdk/puresystem_sdk");
 var PureSystemSdk = puresystem_sdk_1.puresystem_sdk.PureSystemSdk;
 let sdk = PureSystemSdk.instance;
